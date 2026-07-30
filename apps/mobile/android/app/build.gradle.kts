c99bdaad21b060c98b2a284d5ea89c1bebac5cab
Dan <daniel_munoz@live.com.au>
2026-07-30 16:34:54 +1000

    chore(mobile): align bundle IDs to app.catspot.mobile (iOS + Android) (#8)
    Co-authored-by: Dan (via SOSO) <dan@clawd.bot>

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "app.catspot.catspot_mobile"
+    namespace = "app.catspot.mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "app.catspot.catspot_mobile"
+        applicationId = "app.catspot.mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
